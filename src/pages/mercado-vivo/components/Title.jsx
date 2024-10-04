6e9adacd287d5877d515c2993b6e18a4b2a39a95
joaonative <jestudos127@gmail.com>
2024-10-04 14:28:38 -0300

    feat: title component

---
+++ src/pages/mercado-vivo/components/Title.jsx
@@ -0,0 +1,20 @@
+import PropTypes from "prop-types";
+
+const Title = ({ children, start }) => {
+  return (
+    <h2
+      className={`text-2xl lg:text-4xl font-medium font-dm ${
+        start ? "text-start" : "text-center"
+      }`}
+    >
+      {children}
+    </h2>
+  );
+};
+
+Title.propTypes = {
+  children: PropTypes.node.isRequired,
+  start: PropTypes.bool,
+};
+
+export default Title;
