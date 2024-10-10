fb9e47461e873cfaf7b27be4647b617172765b93
joaonative <jestudos127@gmail.com>
2024-10-10 15:51:23 -0300

    data for wine page

---
+++ src/pages/tinto/data.js
@@ -0,0 +1,59 @@
+export const reviews = [
+  {
+    imageUrl:
+      "https://images.unsplash.com/photo-1562788869-4ed32648eb72?q=80&w=1472&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    name: "Eduardo S.",
+    stars: 5,
+    color: "#FE5F55",
+    message:
+      "Excelente clube de vinhos! Recebo mensalmente rótulos cuidadosamente selecionados, que me surpreendem pela qualidade e diversidade.",
+  },
+  {
+    imageUrl:
+      "https://images.unsplash.com/photo-1469334031218-e382a71b716b?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    name: "Juliana M.",
+    stars: 5,
+    color: "#F7CE5B",
+    message:
+      "Adoro a seleção de vinhos, sempre com novidades interessantes. O atendimento é ótimo e sempre consigo tirar dúvidas sobre harmonização.",
+  },
+  {
+    imageUrl:
+      "https://images.unsplash.com/photo-1445053023192-8d45cb66099d?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    name: "Camila T.",
+    stars: 5,
+    color: "#0ACDFF",
+    message:
+      "Assinar o Tinto foi uma das melhores escolhas que fiz! Recebo vinhos incríveis todo mês e aprendi muito mais sobre as diferentes uvas e estilos.",
+  },
+];
+
+export const wines = [
+  {
+    id: 1,
+    imageUrl: "/tinto/1.webp",
+    name: "2021 Illusion Muscat",
+    locale: "US",
+    price: 99.99,
+    oldPrice: 119.99,
+    stars: 5,
+  },
+  {
+    id: 2,
+    imageUrl: "/tinto/2.webp",
+    name: "2022 Fogbow Noir",
+    locale: "US",
+    price: 119.99,
+    oldPrice: 149.99,
+    stars: 5,
+  },
+  {
+    id: 3,
+    imageUrl: "/tinto/3.webp",
+    name: "2022 Opposite the Sun Chardonnay",
+    locale: "US",
+    price: 89.99,
+    oldPrice: 99.99,
+    stars: 5,
+  },
+];
